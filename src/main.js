af4095a62bf3eb60fec622f23f16c2745e19e3b8
dangnh <dang.nh.aprotrain@gmail.com>
2022-09-17 17:08:09 +0700

    add vuex by type vue add vuex

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import i18n from '@/plugins/i18n/index.js'
 import '@/plugins/validation'
 import router from './router'
 import * as filters from './filters' // global filters
+import store from './store'
 
 // register global utility filters
 Object.keys(filters).forEach(key => {
@@ -13,12 +14,12 @@ Object.keys(filters).forEach(key => {
 
 Vue.config.productionTip = false
 console.log('NODE_ENV:', process.env.NODE_ENV);
-console.log('ENV:', process.env.ENV);
 console.log('VUE APP BASE API', process.env.VUE_APP_BASE_API);
 
 new Vue({
   vuetify,
   i18n,
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
